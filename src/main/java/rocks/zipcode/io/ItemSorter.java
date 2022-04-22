271eca36e8d975961370759bffa98e3969b55c82
John-Feil <johnathancfeil@gmail.com>
2022-04-22 15:18:15 -0400

    Finished

---
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -0,0 +1,21 @@
+package rocks.zipcode.io;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author leon on 30/01/2019.
+ */
+public class ItemSorter {
+    Item[] items;
+
+    public ItemSorter(Item[] items) {
+        this.items = items;
+    }
+
+    public Item[] sort(Comparator<Item> comparator) {
+        Arrays.sort(items,comparator);
+        return items;
+    }
+}
